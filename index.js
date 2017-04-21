f0c141cb3be1a1abf5d6205d8380868d1fc67ea7
Unknown <breaksreak@gmail.com>
2017-04-21 19:45:48 -0400

    404 and 500

--- index.js
+++ index.js
@@ -4,6 +4,19 @@ var app = express();
 
 app.set('port', process.env.PORT || 3000);
 
+//404
+app.use(function(req, res){
+  res.type('text/plain');
+  res.status(404);
+  res.send('404 Error');
+});
+//500
+app.use(function(req, res){
+  res.type('text/plain');
+  res.status(500);
+  res.send('500 Error');
+});
+
 app.listen(app.get('port'), function(){
   console.log('ㅁㄴㅇㄹ');
 });
