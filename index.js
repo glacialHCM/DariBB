a2beba13a19a51f36d581c9aa37b0d25607ff17c
Unknown <breaksreak@gmail.com>
2017-04-21 21:15:42 -0400

    으아악끄아악

--- index.js
+++ index.js
@@ -1,25 +1,32 @@
 var express = require('express')
 
-var handlebars = require('express-handlebars').create({ defaultLayout:'main' });
-app.engine('handlebars', handlebars.engine);
-app.set('vie engine', 'handlebars');
-
 var app = express();
+var handlebars = require('express-handlebars')
+  .create({ defaultLayout:'main' });
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
 
 app.set('port', process.env.PORT || 3000);
 
+app.get('/', function(req, res) {
+  res.render('mainpage', { title: '메인 페이지' });
+});
+
+
 //404
-app.use(function(req, res){
+app.use(function(req, res, next){
   res.type('text/plain');
   res.status(404);
   res.send('404 Error');
 });
 //500
-app.use(function(req, res){
+app.use(function(err, req, res, next){
   res.type('text/plain');
   res.status(500);
   res.send('500 Error');
 });
+
+//Section Divider
 
 app.listen(app.get('port'), function(){
   console.log('ㅁㄴㅇㄹ');
